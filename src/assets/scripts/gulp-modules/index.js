8e59c935226510df372e00d94a9218266e48607a
vadymshukr <vadymshukr@gmail.com>
2021-03-31 13:45:55 +0300

    css and js fixes

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -541,9 +541,14 @@ class TabChange {
     setTabHeight(activeTab, container) {
       const activeTabItem = document.querySelector(activeTab);
       const containerItem = document.querySelector(container);
-      const tabHeight = activeTabItem.offsetHeight;
-      containerItem.style.height = `${tabHeight}px`;
-      if ($(window).width() > 1025){
+      setTimeout(function(){
+        const tabHeight = activeTabItem.offsetHeight;
+        containerItem.style.height = `${tabHeight}px`;
+        console.log(activeTabItem)
+        console.log(tabHeight)
+      }, 0)
+
+      if ($(window).width() > 1024){
           window.locoScroll.update();
       }
     }
@@ -561,7 +566,7 @@ class TabChange {
         tabBtns.forEach((btn, _) => {
             btn.addEventListener('click', function (e) {
             e.preventDefault();
-            if ($(window).width() > 1025){
+            if ($(window).width() > 1024){
                 self.removeClass(tabBtns, 'active');
                 self.removeClass(tabs, 'active');
                 this.classList.add('active');
@@ -570,17 +575,19 @@ class TabChange {
                 activeTab.classList.add('active');
                 self.setTabHeight(`${tabClass}.active`, containerClass);
                 window.locoScroll.update();
+            } else{
+                self.removeClass(tabBtns, 'active');
+                self.removeClass(tabs, 'active');
+                $(tabs).hide();
+                this.classList.add('active');
+                const activeHref = this.getAttribute('href');
+                const activeTab = document.querySelector(`[data-tab="${activeHref}"]`);
+                activeTab.classList.add('active');
+                $(`[data-tab="${activeHref}"]`).show();
+                self.setTabHeight(`${tabClass}.active`, containerClass);
             }
             
-            self.removeClass(tabBtns, 'active');
-            self.removeClass(tabs, 'active');
-            $(tabs).hide();
-            this.classList.add('active');
-            const activeHref = this.getAttribute('href');
-            const activeTab = document.querySelector(`[data-tab="${activeHref}"]`);
-            activeTab.classList.add('active');
-            $(`[data-tab="${activeHref}"]`).show();
-            self.setTabHeight(`${tabClass}.active`, containerClass);
+
             })
         })
     }
